411b4baf3d644a480639dab485f33610059f58f9
yohananbengad3 <yohananbengad3@gmail.com>
2026-04-29 16:29:31 -0400

    Add files via upload

---
+++ Face-Morphing/src/mainFolder/runner.java
@@ -0,0 +1,11 @@
+package mainFolder;
+
+import java.io.IOException;
+
+public class runner{
+	public static void main(String args[]){
+		GuiObamaBond morph = null;
+		morph = new GuiObamaBond();
+		morph.setVisible(true);
+	}
+}
